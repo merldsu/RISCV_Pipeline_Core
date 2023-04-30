d231c68de1cc0061b4184b8abc6d64654a293b7f
hamzashabbir517 <shabbirhamza517@gmail.com>
2023-04-30 12:54:28 +0500

    Initial Commit

---
+++ src/Decode_Cyle.v
@@ -0,0 +1,128 @@
+// Copyright 2023 MERL-DSU
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+`include "Control_Unit_Top.v"
+`include "Register_File.v"
+`include "Sign_Extend.v"
+
+module decode_cycle(clk, rst, InstrD, PCD, PCPlus4D, RegWriteW, RDW, ResultW, RegWriteE, ALUSrcE, MemWriteE, ResultSrcE,
+    BranchE,  ALUControlE, RD1_E, RD2_E, Imm_Ext_E, RD_E, PCE, PCPlus4E);
+
+    // Declaring I/O
+    input clk, rst, RegWriteW;
+    input [4:0] RDW;
+    input [31:0] InstrD, PCD, PCPlus4D, ResultW;
+
+    output RegWriteE,ALUSrcE,MemWriteE,ResultSrcE,BranchE;
+    output [2:0] ALUControlE;
+    output [31:0] RD1_E, RD2_E, Imm_Ext_E;
+    output [4:0] RD_E;
+    output [31:0] PCE, PCPlus4E;
+
+    // Declare Interim Wires
+    wire RegWriteD,ALUSrcD,MemWriteD,ResultSrcD,BranchD;
+    wire [1:0] ImmSrcD;
+    wire [2:0] ALUControlD;
+    wire [31:0] RD1_D, RD2_D, Imm_Ext_D;
+
+    // Declaration of Interim Register
+    reg RegWriteD_r,ALUSrcD_r,MemWriteD_r,ResultSrcD_r,BranchD_r;
+    reg [2:0] ALUControlD_r;
+    reg [31:0] RD1_D_r, RD2_D_r, Imm_Ext_D_r;
+    reg [4:0] RD_D_r;
+    reg [31:0] PCD_r, PCPlus4D_r;
+
+
+    // Initiate the modules
+    // Control Unit
+    Control_Unit_Top control (
+                            .Op(InstrD[6:0]),
+                            .RegWrite(RegWriteD),
+                            .ImmSrc(ImmSrcD),
+                            .ALUSrc(ALUSrcD),
+                            .MemWrite(MemWriteD),
+                            .ResultSrc(ResultSrcD),
+                            .Branch(BranchD),
+                            .funct3(InstrD[14:12]),
+                            .funct7(InstrD[31:25]),
+                            .ALUControl(ALUControlD)
+                            );
+
+    // Register File
+    Register_File rf (
+                        .clk(clk),
+                        .rst(rst),
+                        .WE3(RegWriteW),
+                        .WD3(ResultW),
+                        .A1(InstrD[19:15]),
+                        .A2(InstrD[24:20]),
+                        .A3(RDW),
+                        .RD1(RD1_D),
+                        .RD2(RD2_D)
+                        );
+
+    // Sign Extension
+    Sign_Extend extension (
+                        .In(InstrD[31:0]),
+                        .Imm_Ext(Imm_Ext_D),
+                        .ImmSrc(ImmSrcD)
+                        );
+
+    // Declaring Register Logic
+    always @(posedge clk or negedge rst) begin
+        if(rst == 1'b0) begin
+            RegWriteD_r <= 1'b0;
+            ALUSrcD_r <= 1'b0;
+            MemWriteD_r <= 1'b0;
+            ResultSrcD_r <= 1'b0;
+            BranchD_r <= 1'b0;
+            ALUControlD_r <= 3'b000;
+            RD1_D_r <= 32'h00000000; 
+            RD2_D_r <= 32'h00000000; 
+            Imm_Ext_D_r <= 32'h00000000;
+            RD_D_r <= 5'h00;
+            PCD_r <= 32'h00000000; 
+            PCPlus4D_r <= 32'h00000000;
+        end
+        else begin
+            RegWriteD_r <= RegWriteD;
+            ALUSrcD_r <= ALUSrcD;
+            MemWriteD_r <= MemWriteD;
+            ResultSrcD_r <= ResultSrcD;
+            BranchD_r <= BranchD;
+            ALUControlD_r <= ALUControlD;
+            RD1_D_r <= RD1_D; 
+            RD2_D_r <= RD2_D; 
+            Imm_Ext_D_r <= Imm_Ext_D;
+            RD_D_r <= InstrD[11:7];
+            PCD_r <= PCD; 
+            PCPlus4D_r <= PCPlus4D;
+        end
+    end
+
+    // Output asssign statements
+    assign RegWriteE = RegWriteD_r;
+    assign ALUSrcE = ALUSrcD_r;
+    assign MemWriteE = MemWriteD_r;
+    assign ResultSrcE = ResultSrcD_r;
+    assign BranchE = BranchD_r;
+    assign ALUControlE = ALUControlD_r;
+    assign RD1_E = RD1_D_r;
+    assign RD2_E = RD2_D_r;
+    assign Imm_Ext_E = Imm_Ext_D_r;
+    assign RD_E = RD_D_r;
+    assign PCE = PCD_r;
+    assign PCPlus4E = PCPlus4D_r;
+
+endmodule
--- src/Sign_Extend.v
+++ src/Sign_Extend.v
@@ -12,13 +12,12 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-module Sign_Extend (In,Imm_Ext,ImmSrc);
+module Sign_Extend (In,ImmSrc,Imm_Ext);
+    input [31:0] In;
+    input [1:0] ImmSrc;
+    output [31:0] Imm_Ext;
 
-    input [31:0]In;
-    input ImmSrc;
-    output [31:0]Imm_Ext;
+    assign Imm_Ext =  (ImmSrc == 2'b00) ? {{20{In[31]}},In[31:20]} : 
+                     (ImmSrc == 2'b01) ? {{20{In[31]}},In[31:25],In[11:7]} : 32'h00000000; 
 
-    assign Imm_Ext = (ImmSrc == 1'b1) ? ({{20{In[31]}},In[31:25],In[11:7]}):
-                                        {{20{In[31]}},In[31:20]};
-                                
 endmodule
