b02afaae7b156cd1ccb460ff6791e69d9db3e890
hamzashabbir517 <shabbirhamza517@gmail.com>
2023-05-02 20:49:26 +0500

    Initial Commit

---
+++ src/Execute_Cycle.v
@@ -0,0 +1,104 @@
+// Copyright 2023 MERL-DSU
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+`include "ALU.v"
+`include "Mux.v"
+`include "PC_Adder.v"
+
+module execute_cycle(clk, rst, RegWriteE, ALUSrcE, MemWriteE, ResultSrcE, BranchE, ALUControlE, 
+    RD1_E, RD2_E, Imm_Ext_E, RD_E, PCE, PCPlus4E, PCSrcE, PCTargetE, RegWriteM, MemWriteM, ResultSrcM, RD_M, PCPlus4M, WriteDataM, ALU_ResultM);
+
+    // Declaration I/Os
+    input clk, rst, RegWriteE,ALUSrcE,MemWriteE,ResultSrcE,BranchE;
+    input [2:0] ALUControlE;
+    input [31:0] RD1_E, RD2_E, Imm_Ext_E;
+    input [4:0] RD_E;
+    input [31:0] PCE, PCPlus4E;
+
+    output PCSrcE, RegWriteM, MemWriteM, ResultSrcM;
+    output [4:0] RD_M; 
+    output [31:0] PCPlus4M, WriteDataM, ALU_ResultM;
+    output [31:0] PCTargetE;
+
+    // Declaration of Interim Wires
+    wire [31:0] Src_B;
+    wire [31:0] ResultE;
+    wire ZeroE;
+
+    // Declaration of Register
+    reg RegWriteE_r, MemWriteE_r, ResultSrcE_r;
+    reg [4:0] RD_E_r;
+    reg [31:0] PCPlus4E_r, RD2_E_r, ResultE_r;
+
+    // Declaration of Modules
+    // ALU Src Mux
+    Mux alu_src_mux (
+            .a(RD2_E),
+            .b(Imm_Ext_E),
+            .s(ALUSrcE),
+            .c(Src_B)
+            );
+
+    // ALU Unit
+    ALU alu (
+            .A(RD1_E),
+            .B(Src_B),
+            .Result(ResultE),
+            .ALUControl(ALUControlE),
+            .OverFlow(),
+            .Carry(),
+            .Zero(ZeroE),
+            .Negative()
+            );
+
+    // Adder
+    PC_Adder branch_adder (
+            .a(PCE),
+            .b(Imm_Ext_E),
+            .c(PCTargetE)
+            );
+
+    // Register Logic
+    always @(posedge clk or negedge rst) begin
+        if(rst == 1'b0) begin
+            RegWriteE_r <= 1'b0; 
+            MemWriteE_r <= 1'b0; 
+            ResultSrcE_r <= 1'b0;
+            RD_E_r <= 5'h00;
+            PCPlus4E_r <= 32'h00000000; 
+            RD2_E_r <= 32'h00000000; 
+            ResultE_r <= 32'h00000000;
+        end
+        else begin
+            RegWriteE_r <= RegWriteE; 
+            MemWriteE_r <= MemWriteE; 
+            ResultSrcE_r <= ResultSrcE;
+            RD_E_r <= RD_E;
+            PCPlus4E_r <= PCPlus4E; 
+            RD2_E_r <= RD2_E; 
+            ResultE_r <= ResultE;
+        end
+    end
+
+    // Output Assignments
+    assign PCSrcE = ZeroE &  BranchE;
+    assign RegWriteM = RegWriteE;
+    assign MemWriteM = MemWriteE;
+    assign ResultSrcM = ResultSrcE;
+    assign RD_M = RD_E;
+    assign PCPlus4M = PCPlus4E;
+    assign WriteDataM = RD2_E;
+    assign ALU_ResultM = ResultE_r;
+
+endmodule
